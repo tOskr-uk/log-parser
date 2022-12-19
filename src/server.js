62ddc8aa204c549a1150a8a54edfafa03c8bb1f4
toskr <dccconnect@gmail.com>
2022-12-19 12:23:56 +0000

    some minor file reorg and installed and configured ejs templating engine

--- src/server.js
+++ src/server.js
@@ -3,7 +3,15 @@ const Log = require('./model');
 require('./db/mongoose') // runs the file directly from here
 
 const app = express();
+app.set('view engine', 'ejs')
+
+
 app.use(express.json());
+
+
+app.get('/',(req, res)=>{
+    res.render('../public/index');
+})
 
 app.get('/data', async(req, res)=>{
     try{
